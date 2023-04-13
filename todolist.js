00a57172f37f851a6e7892f44dad0fbee8bac1c5
yupm15 <127378148+yupm15@users.noreply.github.com>
2023-04-13 18:09:23 +0800

    Add files via upload

---
+++ todolist.js
@@ -0,0 +1,30 @@
+function add(e)
+{
+    var text=document.getElementsByTagName('input')[0];
+    var nd=document.createElement('li');
+    var nb=document.createElement('button');
+    
+    nb.setAttribute('class','delete');
+    nb.setAttribute('style','margin-left: 10px;');
+    nd.innerText=text.value;
+    nb.innerText="Delete"
+    nd.append(nb);
+    document.getElementById('u1').append(nd); 
+    nb.addEventListener("click",remove)
+    text.value="";
+}
+function remove(e)
+{
+    
+    //e.target.remove();
+    var li=e.target.parentNode;
+    li.parentNode.removeChild(li);
+
+}
+var btnA = document.getElementsByClassName('add')[0];
+btnA.addEventListener("click",add)
+//btnD=document.getElementsByClassName('delete')[0];
+//btnD.removeEventListener("click",remove)
+
+
+
